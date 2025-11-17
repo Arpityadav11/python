b5a6d4ca159ed80111a8b5c3343b169901f9fc73
Arpityadav11 <arpityadav112005@gmail.com>
2025-11-17 10:21:01 +0530

    added the remaining files

---
+++ Datascience/numpy.py
@@ -0,0 +1,13 @@
+'''
+Numpy library 
+
+Q. What is numpy?
+
+how to use numpy - we have to import the package numpy 
+e.g - import numpy as np (as is used to give the short name here the short name is np)
+
+creating the array using the library
+data = np.array([1,2,3,4,5])
+1. Array is very fast as compare to list 
+2. it uses less memory 
+'''
